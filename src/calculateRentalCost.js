b47ec195758da116a69b433e923cb758eb9d7b8f
vladahvala <80634246+vladahvala@users.noreply.github.com>
2025-12-18 21:47:04 +0200

    mplement calculateentalost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
-}
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+  const DAY_PRICE = 40;
 
+  let totalPrice = days * DAY_PRICE;
+
+  if (days >= LONG_TERM) {
+    totalPrice -= LONG_TERM_DISCOUNT;
+  } else if (days >= SHORT_TERM) {
+    totalPrice -= SHORT_TERM_DISCOUNT;
+  }
+
+  return totalPrice;
+}
 module.exports = calculateRentalCost;
